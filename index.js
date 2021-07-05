5fd46f63279117b096ee308075ae5fa965078fdf
Renan Souza <renantrsouza@gmail.com>
2021-07-05 11:14:36 -0400

    ajuste index.js file, removi adcionei o método sacar na ContaCorrente.js file

--- index.js
+++ index.js
@@ -6,19 +6,10 @@ import {ContaPoupanca} from "./ContaPoupanca.js";
 const cliente1 = new Cliente("Ricardo", 11122233309);
 const conta1 = new ContaCorrente(cliente1, 103);
 conta1.depositar(200);
-
-
-//Cliente 2
-const cliente2 = new Cliente("Alice", 88822233309);
-const conta2 = new ContaCorrente(cliente2, 102);
-conta2.depositar(50);
-
-const cliente3 = new Cliente("Rex", 66355323);
-const conta3 = new ContaCorrente(cliente3, 103);
-const contap3 = new ContaPoupanca(200, cliente3, 103);
+conta1.sacar(100);
 
 //Output
 //conta1.transfere(50, conta2);
-console.log(contap3);
+console.log(conta1);
 //console.log(ContaCorrente.numeroDeContas);
 
