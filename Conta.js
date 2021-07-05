2c1f0bb49aefb19337f58496a5da318f476aa8d9
Renan Souza <renantrsouza@gmail.com>
2021-07-05 10:53:28 -0400

    add extend nas classes ContaCorrente e ContaPoupanca

--- Conta.js
+++ Conta.js
@@ -1,8 +1,23 @@
+import { Cliente } from "./Cliente.js";
+
 export class Conta{
     constructor(saldoInicial, cliente, agencia){
         this._saldo = saldoInicial;
         this._cliente = cliente;
         this._agencia = agencia;
+    }
+
+    set cliente(novoCliente){
+        if(novoCliente instanceof Cliente){
+            this._cliente = novoCliente;
+        }
+    }
+    get cliente(){
+        return this._cliente;
+    }
+    
+    get saldo(){
+        return this._saldo;
     }
 
     sacar(valor){
--- ContaPoupanca.js
+++ ContaPoupanca.js
@@ -1,47 +1,7 @@
-import {Cliente} from "./Cliente.js";
+import { Conta } from "./Conta.js";
 
-export class ContaPoupanca{
+export class ContaPoupanca extends Conta{
     constructor(saldoInicial, cliente, agencia){
-        this._saldo = saldoInicial;
-        this.cliente = cliente;
-        this._agencia = agencia
-    }
-
-    /*Acessores*/
-    get saldo(){
-        return this._saldo;
-    }
-
-    set cliente(novoCliente){
-        if(novoCliente instanceof Cliente){
-            this._cliente = novoCliente;
-        }
-    }
-    get cliente(){
-        return this._cliente;
-    }
-
-    set agencia(novaAgencia){
-        this._agencia = novaAgencia;
-    }
-    get agencia(){
-        return this._agencia;
-    }
-    
-    /*Métodos*/
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-    depositar(valor){
-        if(valor > 0){
-            this._saldo += valor;
-        }
-    }
-    transfere(valor, conta){
-        const valorSacado = this.sacar(valor);
-        conta.depositar(valorSacado);
+        super(saldoInicial, cliente, agencia);
     }
 }
